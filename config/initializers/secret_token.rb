b3afbb3e425b65dfe9f42066e0e3efd37e685cde
Joseph Ngo <joengo83@gmail.com>
2012-12-29 13:59:24 -0800

    Inital Hartl demo_app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '36c89039f8102a30f589be4477ac29187cb5ad6c246be84bfff79d10985226c3ad333118b0e8b5320406b30fb42ba34db3168bd322550078e6f82870673aef2f'
